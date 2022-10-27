ef40b7b0979c8060b4033d808a8e091a9ec2db25
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-10-27 14:53:26 -0400

    Create skeleton of app, add api credentials.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css'
 
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall'
+const API_KEY = '580363fa8ea6956daf32ba04f6aa5e61'
+
 function App () {
-  return <div className='App'>Test</div>
+  return (
+    <div className='App'>
+      <Navbar />
+      <Weather />
+    </div>
+  )
+}
+
+function Navbar () {
+  return <div className='Navbar'></div>
+}
+
+function Weather () {
+  return <div className='Weather'></div>
 }
 
 export default App
